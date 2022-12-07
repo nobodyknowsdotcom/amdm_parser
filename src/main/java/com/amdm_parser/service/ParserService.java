ee12c49dcc9c2231443e3c7612d8cea2ff6bf196
Dmitry Medvedev <myclosestisa@gmail.com>
2022-12-07 18:59:54 +0500

    Create inner song position counter

--- src/main/java/com/amdm_parser/service/ParserService.java
+++ src/main/java/com/amdm_parser/service/ParserService.java
@@ -8,7 +8,6 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import org.jsoup.select.Selector;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
@@ -19,9 +18,6 @@ import java.util.Locale;
 @Slf4j
 @Service
 public class ParserService {
-    @Value("${parser.songsOnPage}")
-    private int songsOnPage;
-
     /*
     Принимает категорию и отдает список песен со всех страниц каталога
     */
@@ -35,7 +31,7 @@ public class ParserService {
         for (int i = 0; i < pagesCount; i++) {
             Document page = getPage(category.getUrl()+String.format("/page%s", i+1));
             Element songsContainer = getSongsContainer(page);
-            result.addAll(getSongsListFromContainer(songsContainer, i, category));
+            result.addAll(getSongsListFromContainer(songsContainer, category));
         }
 
         log.info(String.format("Got %s songs by %s, pages count: %s", result.size(),
@@ -77,17 +73,20 @@ public class ParserService {
     /*
     * Вытаскивает из элемента-родителя весь список песен и возвращает как ArrayList
     */
-    private ArrayList<Song> getSongsListFromContainer(Element parentContainer, int page, TopicCategories category){
+    private ArrayList<Song> getSongsListFromContainer(Element parentContainer, TopicCategories category){
         ArrayList<Song> result = new ArrayList<>();
         Elements songElements = parentContainer.select("td.artist_name");
-
-        for(int i=0; i<songElements.size(); i++){
+        /*
+        * Создаем отдельный счетчик вне цикла, т.к. при возникновении SelectorParseException
+        * будут разрывы в нумерации песен
+        */
+        int position = 0;
+        for (Element songElement : songElements) {
             try { // Передаем в метод Element (будущий Song), место в списке и категорию
-                int songIndex = (page*songsOnPage)+i;
-                Song parsedSong = parseElementIntoSong(songElements.get(i), songIndex, category);
+                Song parsedSong = parseElementIntoSong(songElement, position, category);
                 result.add(parsedSong);
-            }
-            catch (Selector.SelectorParseException e){
+                position += 1;
+            } catch (Selector.SelectorParseException e) {
                 log.error("Can't parse song!");
                 e.printStackTrace();
             }
