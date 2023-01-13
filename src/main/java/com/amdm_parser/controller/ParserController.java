3d3d7280518d632d762747f79c2a15f87379f0fc
nobodyknowsdotcom <nobodyknowsdotcom@gmail.com>
2023-01-13 21:25:37 +0500

    Log everything on /{topicCategory} endpoint

--- src/main/java/com/amdm_parser/controller/ParserController.java
+++ src/main/java/com/amdm_parser/controller/ParserController.java
@@ -4,12 +4,14 @@ import com.amdm_parser.dto.TopicPage;
 import com.amdm_parser.service.PaginationService;
 import com.amdm_parser.utils.SortCategories;
 import com.amdm_parser.utils.TopicCategories;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class ParserController {
     final PaginationService paginationService;
 
@@ -20,6 +22,7 @@ public class ParserController {
     @GetMapping("/{topicCategory}")
     public TopicPage getSongsPagination(@PathVariable TopicCategories topicCategory, @RequestParam int page,
                                         @RequestParam int size, @RequestParam SortCategories sort){
+        log.info(String.format("Got request /%s?page=%s&size=%s&sort=%s", topicCategory, page, size, sort));
         return paginationService.getTopicPage(topicCategory, size, page, sort);
     }
 }
